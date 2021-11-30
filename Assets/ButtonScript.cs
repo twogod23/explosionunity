53f37090403d322a36a5eafd728c3025aafae120
Futagami <60266261.futagami.cgp@gmail.com>
2021-11-30 13:18:59 +0900

    Initial commit

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    //ボタンが押されたときに呼び出される関数
+    public void Select()
+    {
+        Debug.Log("explosion"); 
+    }
+}
